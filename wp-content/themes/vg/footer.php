22496b4cb445e3703af607ed6358510c2498d9ad
minhtuanitus <daominhtuan1995@gmail.com>
2018-04-11 00:00:22 +0700

    up

--- wp-content/themes/vg/footer.php
+++ wp-content/themes/vg/footer.php
@@ -33,6 +33,35 @@
 	        }
 	    	?>
 	    </div>
+
+<!-- Contact Widget (Chat FB, Chat Zalo, Goi Hotline giông orchard.vn -->
+<div class="online-support">
+    <div class="dropup force-open">
+        <ul class="dropdown-2-menu dropdown-2-menu-right dropdown-2--support">
+            <li>
+                <a href="tel:12345678">
+                    <i class="icon-icon-phone"></i> GỌI HOTLINE
+                </a>
+                <ul>
+                    <li><a target="_blank" href="#">Nội dung 1</a></li>
+                    <li><a target="_blank" href="#">Nội dung 2</a></li>
+                    <li><a target="_blank" href="#">Nội dung 3</a></li>
+                    <li><a target="_blank" href="#">Nội dung 4</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://m.me/vuagym" target="_blank" rel="noopener">
+                    <i class="icon-icon-chat"></i>
+                    CHAT FB</a>
+            </li>
+            <li>
+                <a href="http://zalo.me/2275406627518748561" target="_blank" rel="noopener">
+                    <i class="icon-icon-zalo"></i> CHAT ZALO
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
 	<div id="boxes"></div>
         
 	<?php wp_footer(); ?>
