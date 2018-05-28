5a2732165e0de4c0cc9ed2dc48207c342eed2faf
minhtuanitus <daominhtuan1995@gmail.com>
2018-05-29 06:57:55 +0700

    up

--- wp-content/plugins/ka-pos/ka-pos.php
+++ wp-content/plugins/ka-pos/ka-pos.php
@@ -522,50 +522,77 @@ function function_testing_page() {
     
     echo "TESTING PAGE";
 
-    $spreadsheet = new Spreadsheet();
+    $upload = wp_upload_dir();
+    $upload_dir = $upload['basedir'];
+    $upload_dir = $upload_dir . '/import-files/';
+    
+    $file_input = $upload_dir . 'nhap_hang_mau.xlsx';
+    $file_output = $upload_dir . 'nhap_hang_mau_out.xlsx';
+    
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+    
+    $spreadsheet = $reader->load($file_input);
+    $spreadsheet->getActiveSheet()->removeColumn('C', 3);
+    $spreadsheet->getActiveSheet()->removeRow(1);
+    
+    $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, "Xlsx");
+    $writer->save($file_output);
+    
+//    echo '<pre>';
+//    print_r($spreadsheet);
+//    echo '<pre>';
+    exit;
+//
+//    //Specify the properties for this document
+//    $spreadsheet->getProperties()
+//        ->setTitle('PHP Download Example')
+//        ->setSubject('A PHPExcel example')
+//        ->setDescription('A simple example for PhpSpreadsheet. This class replaces the PHPExcel class')
+//        ->setCreator('php-download.com')
+//        ->setLastModifiedBy('php-download.com');
+//
+//    //Adding data to the excel sheet
+//    $spreadsheet->setActiveSheetIndex(0)
+//        ->setCellValue('A1', 'This')
+//        ->setCellValue('A2', 'is')
+//        ->setCellValue('A3', 'only')
+//        ->setCellValue('A4', 'an')
+//        ->setCellValue('A5', 'example');
+//
+//    $spreadsheet->getActiveSheet()
+//        ->setCellValue('B1', "You")
+//        ->setCellValue('B2', "can")
+//        ->setCellValue('B3', "download")
+//        ->setCellValue('B4', "this")
+//        ->setCellValue('B5', "library")
+//        ->setCellValue('B6', "on")
+//        ->setCellValue('B7', "https://php-download.com/package/phpoffice/phpspreadsheet");
+//
+//
+//    $spreadsheet->getActiveSheet()
+//        ->setCellValue('C1', 1)
+//        ->setCellValue('C2', 0.5)
+//        ->setCellValue('C3', 0.25)
+//        ->setCellValue('C4', 0.125)
+//        ->setCellValue('C5', 0.0625);
+//
+//    $spreadsheet->getActiveSheet()
+//        ->setCellValue('C6', '=SUM(C1:C5)');
+//    $spreadsheet->getActiveSheet()
+//        ->getStyle("C6")->getFont()
+//        ->setBold(true);
 
-    //Specify the properties for this document
-    $spreadsheet->getProperties()
-        ->setTitle('PHP Download Example')
-        ->setSubject('A PHPExcel example')
-        ->setDescription('A simple example for PhpSpreadsheet. This class replaces the PHPExcel class')
-        ->setCreator('php-download.com')
-        ->setLastModifiedBy('php-download.com');
 
-    //Adding data to the excel sheet
-    $spreadsheet->setActiveSheetIndex(0)
-        ->setCellValue('A1', 'This')
-        ->setCellValue('A2', 'is')
-        ->setCellValue('A3', 'only')
-        ->setCellValue('A4', 'an')
-        ->setCellValue('A5', 'example');
-
-    $spreadsheet->getActiveSheet()
-        ->setCellValue('B1', "You")
-        ->setCellValue('B2', "can")
-        ->setCellValue('B3', "download")
-        ->setCellValue('B4', "this")
-        ->setCellValue('B5', "library")
-        ->setCellValue('B6', "on")
-        ->setCellValue('B7', "https://php-download.com/package/phpoffice/phpspreadsheet");
-
-
-    $spreadsheet->getActiveSheet()
-        ->setCellValue('C1', 1)
-        ->setCellValue('C2', 0.5)
-        ->setCellValue('C3', 0.25)
-        ->setCellValue('C4', 0.125)
-        ->setCellValue('C5', 0.0625);
-
-    $spreadsheet->getActiveSheet()
-        ->setCellValue('C6', '=SUM(C1:C5)');
-    $spreadsheet->getActiveSheet()
-        ->getStyle("C6")->getFont()
-        ->setBold(true);
-
-
-    $writer = IOFactory::createWriter($spreadsheet, "Xlsx"); //Xls is also possible
-    $writer->save("my_excel_file.xlsx");
+//    $writer = IOFactory::createWriter($spreadsheet, "Xlsx"); //Xls is also possible
+    
+//    $upload = wp_upload_dir();
+//    $upload_dir = $upload['basedir'];
+//    $upload_file_path = $upload_dir . '/import-files/my_excel_file.xlsx';
+    
+//    echo $upload_file_path;
+    
+//    $writer->save($upload_file_path);
+    
 }
 
 function update_default_manual_sync_options() {
